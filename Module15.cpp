d75ef163e0ea8635b517bbd5fe307b9a93675b69
aleksandra_orlova <aleksa.orlik2000@gmail.com>
2023-11-18 15:52:01 +0300

    Добавьте файлы проекта.

---
+++ Module15.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+const int N = 22;
+
+void FindOddNumbers(int limit, bool isOdd)
+{
+    for (int i = isOdd; i <= limit; i+=2)
+    {
+        cout << i << " ";
+    }
+}
+
+int main()
+{
+    FindOddNumbers(N, true);
+    cout << "\n";
+    FindOddNumbers(N, false);
+}
+
